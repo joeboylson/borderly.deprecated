8159049f502b7b08990350508f05d82814454a89
Joe Boylson <joeboylson@gmail.com>
2020-02-23 21:33:10 -0500

    adding production config

--- app.py
+++ app.py
@@ -91,5 +91,8 @@ def upload_file():
 # --------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-  print('\n\n')
-  app.run(debug=True)
+
+  IS_PRODUCTION = os.environ.get('PYTHON_ENV') == 'PRODUCTION'
+  DEBUG = True if not IS_PRODUCTION else False
+  PORT = 5000 if not IS_PRODUCTION else os.environ.get('PORT')
+  app.run(debug=DEBUG, port=PORT)
