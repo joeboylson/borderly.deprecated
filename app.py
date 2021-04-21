3b88989d5c5e0b2d4420ed4236f9e8000fc0d79d
Joe Boylson <jboylson@saec-kv.com>
2021-04-21 17:40:35 -0400

    feat: add discord integration;

--- app.py
+++ app.py
@@ -3,12 +3,14 @@
 import os
 import math
 import json
-import datetime
 import uuid
+import requests
+import threading
+
 from io import BytesIO
+from datetime import datetime
 from flask import Flask, request, redirect, url_for, render_template, send_file, send_from_directory, Blueprint
 from PIL import Image, ImageOps
-
 
 app = Flask(__name__, template_folder='./dist')
 
@@ -44,8 +46,6 @@ def add_border(input_image, output_image, border, color=0):
   returns: None -- saves image to "static/images"
   '''
 
-  print(1)
-
   img = Image.open(input_image)
   w, _ = img.size
 
@@ -62,6 +62,28 @@ def append_suffix(filename):
   returns: filename + "-BORDERLY" + extension
   '''
   return "{0}{2}.{1}".format(*filename.rsplit('.', 1) + ['-BORDERLY'])
+
+def notify_discord(filenames):
+    discord_url = "https://discord.com/api/webhooks/831681436062056508/gePXCk-sjscRfB3A9zzv-LmkVIc2n45B8gTUXH7M75KB8dzed9I-R6QFsZ_F4rD0gNUj"
+    files = {}
+    
+    def _notify_discord():
+        embeds = [{
+          "description": datetime.now().strftime("%m/%d/%Y %H:%M:%S"),
+          "image": {
+            "url": "https://borderly.herokuapp.com/api/static/images/{}".format(filename)
+          }
+        } for filename in filenames]
+
+        json = {
+          "username": "BORDERLY BOT",
+          "embeds": embeds,
+        }
+
+        result = requests.post(discord_url, json=json)
+
+    notification_thread = threading.Thread(target=_notify_discord)
+    notification_thread.start()
 
 
 
@@ -85,7 +107,6 @@ def upload_file():
   ''' POST - recieves image in formdata - calls "addborder()" - returns filename '''  
 
   try:
-
     image_dict = request.files.to_dict()
     filenames = []
 
@@ -96,6 +117,8 @@ def upload_file():
       add_border(input_image=image, output_image=filename, border=5, color='#FFF')
       filenames.append(filename)
 
+    if (IS_PRODUCTION):
+      notify_discord(filenames)
     return json.dumps({'success': True, 'message': 'SUCCESS', 'data': {'filenames': filenames}})
 
   except Exception as e:
